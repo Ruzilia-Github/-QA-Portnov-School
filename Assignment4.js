021b4164eef4cce788eab6d23e98e6446dd9f4e1
----for <ruziliaural@gmail.com>
2023-12-04 21:14:56 -0500

    12.04.23

--- Assignment4.js
+++ Assignment4.js
@@ -26,8 +26,7 @@ function getNumberOfDays(start, end) {
     const oneDay = 1000 * 60 * 60 * 24;
     const diffInTime = date2.getTime() - date1.getTime();
     const diffInDays = Math.round(diffInTime / oneDay);
-    return diffInDays;
-    console.log(`The number of days between ${date1} and ${date2} is: ${diffInDays}`);
+    return `The number of days between ${date1} and ${date2} is: ${diffInDays}`;
 }
 
 
@@ -40,38 +39,27 @@ Refactor calculator with switch operator
 
 Solution:*/
 
-function getDateComponents(dateString) {
-    const [month, day, year] = dateString.split('/').map(Number);
-    return { day, month, year };
-}
-
-function dateDiffInDays(date1, date2) {
-    const d1 = getDateComponents(date1);
-    const d2 = getDateComponents(date2);
-
-    let daysDiff = 0;
+function getNumberOfDays(start, end) {
+    const date1 = new Date(start);
+    const date2 = new Date(end);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const diffInTime = date2.getTime() - date1.getTime();
+    const diffInDays = Math.round(diffInTime / oneDay);
 
     switch (true) {
-        case d1.year !== d2.year:
-            daysDiff += (d2.year - d1.year) * 365;
+        case isNaN(diffInDays):
+            console.log('Invalid date format. Please provide valid date strings.');
+            return null;
+        case diffInDays === 0:
+            console.log('The start and end dates are the same.');
             break;
-
-        case d1.month !== d2.month:
-            daysDiff += (d2.month - d1.month) * 30;
-            break;
-
         default:
-            daysDiff += d2.day - d1.day;
+            console.log(`The number of days between ${date1} and ${date2} is: ${diffInDays}`);
             break;
     }
 
-    return daysDiff;
+    return diffInDays;
 }
 
-// Example usage
-const date1 = '7/9/1981';
-const date2 = '7/9/2000';
+console.log(getNumberOfDays("7/9/1981", "7/9/2000"));
 
-const daysDifference = dateDiffInDays(date1, date2);
-console.log(`The number of days between ${date1} and ${date2} is: ${daysDifference}`);
-//test
